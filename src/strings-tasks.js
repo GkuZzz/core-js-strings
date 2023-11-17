f3c7d98629f730725c9054a099327fb1b79dc833
GkuZzz <117741080+GkuZzz@users.noreply.github.com>
2023-11-17 17:37:03 +0300

    feat: add 1 task

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -361,8 +361,15 @@ function isPalindrome(str) {
  *   findLongestWord('A long and winding road') => 'winding'
  *   findLongestWord('No words here') => 'words'
  */
-function findLongestWord(/* sentence */) {
-  throw new Error('Not implemented');
+function findLongestWord(sentence) {
+  const sentToArr = sentence.split(' ');
+  let longestWord = '';
+  for (let i = 0; i < sentToArr.length; i += 1) {
+    if (sentToArr[i].length > longestWord.length) {
+      longestWord = sentToArr[i];
+    }
+  }
+  return longestWord;
 }
 
 /**
